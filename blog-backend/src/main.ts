180548d1c6d68329d6ae66587ec56117fb65bd4e
belvinb <belvinbbenny@gmail.com>
2022-11-05 00:08:24 +0530

    created production build

--- blog-backend/src/main.ts
+++ blog-backend/src/main.ts
@@ -1,9 +1,15 @@
+const express = require('express')
+const path = require('path');
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+
+  app.use(express.static(path.join(__dirname, '../build')));
+
+
   await app.listen(5000);
 }
 bootstrap();
--- blog-frontend/src/contexts/auth0-context.tsx
+++ blog-frontend/src/contexts/auth0-context.tsx
@@ -1,5 +1,5 @@
 import React,{Component,createContext,useContext} from 'react';
-import {createAuth0Client} from "@auth0/auth0-spa-js";
+import {Auth0ClientOptions, createAuth0Client} from "@auth0/auth0-spa-js";
 import {Auth0Client} from '@auth0/auth0-spa-js';
 
 
@@ -38,10 +38,10 @@ export class Auth0Provider extends Component<any, IState> {
     };
   }
 
-  Auth0ClientOptions = {
+  config: Auth0ClientOptions = {
     domain: `${process.env.REACT_APP_AUTH0_DOMAIN}`,
     clientId: `${process.env.REACT_APP_AUTH0_CLIENT_ID}`,
-    redirectUri: window.location.origin,
+    // redirectUri: window.location.origin,
   };
 
   componentDidMount() {
@@ -51,7 +51,7 @@ export class Auth0Provider extends Component<any, IState> {
   //initialize the auth0 library
 
   initializeAuth0 = async () => {
-    const auth0Client = await createAuth0Client(this.Auth0ClientOptions);
+    const auth0Client = await createAuth0Client(this.config);
     this.setState({ auth0Client });
     //check to see if redirected after login
     if (window.location.search.includes("code=")) {
